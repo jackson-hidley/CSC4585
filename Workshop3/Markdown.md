396d0a43f33485b1359c03d45cd76b34a50a2969
Jackson Hidley <60934202+jackson-hidley@users.noreply.github.com>
2021-09-20 20:07:31 -0500

    Add files via upload

--- Workshop3/Markdown.md
+++ Workshop3/Markdown.md
@@ -1,8 +1,40 @@
-Security weakness name: Hard-coded secert
-Security weakness location: Variable 'X' in line# '19'
-  
-Security weakness name: Admin by default
-Security weakness location: Variable 'X' in line# '19'
-  
-Security weakness name: 
-Security weakness location: Variable 'X' in line# '100'
+Security weakness name: Hard-coded secert  
+Security weakness name: Hard-Coded Secret
+Security weakness location: Varible ' sat_user ' in line# ' 10 '
+Security weakness usage:  Workshop3.values.yaml
+Security weakness name: Admin by Default
+Security weakness location: Varible ' sat_user ' in line# ' 10 '
+Security weakness usage:  Workshop3.values.yaml
+Security weakness name: Hard-Coded Secret
+Security weakness location: Varible ' sat_pass ' in line# ' 11 '
+Security weakness usage:  Workshop3.values.yaml
+Security weakness name: Use of HTTP without SSL/TLS
+Security weakness location: Varible ' foreman_repository_base ' in line# ' 29 '
+Security weakness usage:  Workshop3.values.yaml
+Security weakness name: Use of HTTP without SSL/TLS
+Security weakness location: Varible ' foreman_plugin_repository_base ' in line# ' 30 '
+Security weakness usage:  Workshop3.values.yaml
+Security weakness name: No Integrity Check
+Security weakness location: Varible ' epel_repo_installer ' in line# ' 34 '
+Security weakness usage:  Workshop3.values.yaml
+Security weakness name: Hard-Coded Secret
+Security weakness location: Varible ' katello_user ' in line# ' 36 '
+Security weakness usage:  Workshop3.values.yaml
+Security weakness name: Hard-Coded Secret
+Security weakness location: Varible ' katello_password ' in line# ' 37 '
+Security weakness usage:  Workshop3.values.yaml
+Security weakness name: Use of HTTP without SSL/TLS
+Security weakness location: Varible ' content_rhel_url ' in line# ' 41 '
+Security weakness usage:  Workshop3.values.yaml
+Security weakness name: Use of HTTP without SSL/TLS
+Security weakness location: Varible ' content_sattools_url ' in line# ' 44 '
+Security weakness usage:  Workshop3.values.yaml
+Security weakness name: No Integrity Check
+Security weakness location: Varible ' epel_release_rpm ' in line# ' 68 '
+Security weakness usage:  Workshop3.values.yaml
+Security weakness name: Use of HTTP without SSL/TLS
+Security weakness location: Varible ' test_sync_repositories_url_template ' in line# ' 72 '
+Security weakness usage:  Workshop3.values.yaml
+Security weakness name: Use of HTTP without SSL/TLS
+Security weakness location: Varible ' vms_kss_os ' in line# ' 77 '
+Security weakness usage:  Workshop3.values.yaml
--- Workshop3/Workshop3.py
+++ Workshop3/Workshop3.py
@@ -1,38 +1,51 @@
 import yaml
 
-with open("Workshop3.values.yaml", "r") as stream:
-    try:
-        # print(yaml.safe_load(stream))
-        content = yaml.safe_load(stream)
-        for i, content in enumerate(content):
-            print(i, content)
-    except:
-        print("exc")
 
+def check(file):
+    with open(file, "r") as stream:
+        try:
+            # print(yaml.safe_load(stream))
+            content = yaml.safe_load(stream)
+            for i, content in enumerate(content):
+                if content == "sat_user" or content == "sat_pass" or content == "katello_user" or content == "name" or content == "katello_password" or content == "rootpw" in content:
+                    print("Security weakness name: Hard-Coded Secret")
+                    print("Security weakness location: Varible '", content, "' in line# '", i, "'")
+                    print("Security weakness usage: ", file) 
+                if content == "sat_user" or content == "name" in content:
+                    print("Security weakness name: Admin by Default")
+                    print("Security weakness location: Varible '", content, "' in line# '", i, "'")
+                    print("Security weakness usage: ", file) 
+                if content == "epel_repo_installer" or content == "epel_release_rpm" in content:
+                    print("Security weakness name: No Integrity Check")
+                    print("Security weakness location: Varible '", content, "' in line# '", i, "'")
+                    print("Security weakness usage: ", file) 
+                if content == "foreman_repository_base" or content == "foreman_plugin_repository_base" or content == "content_rhel_url" or content == "content_sattools_url" or content == "test_sync_repositories_url_template" or content == "vms_kss_os" in content:
+                    print("Security weakness name: Use of HTTP without SSL/TLS")
+                    print("Security weakness location: Varible '", content, "' in line# '", i, "'")
+                    print("Security weakness usage: ", file) 
+                if content == "ip" in content:
+                    print("Security weakness name: Unrestricted IP Address")
+                    print("Security weakness location: Varible '", content, "' in line# '", i, "'")
+                    print("Security weakness usage: ", file) 
 
+        except:
+            print("exc")
+
+    stream.close()
+    return 0
+
+file1 = "Workshop3.values.yaml"
+file2 = "Workshop3.play1.yaml"
+file3 = "Workshop3.play2.yaml"
+
+check(file1)
+print("File 1 done")
+check(file2)
+print("File 2 done")
+check(file3)
+print("File 3 done")
 
 
    
-#'''
-#with open("Workshop3.values.yaml", "r") as stream:
-#    lines = []
-#    x = 1
-#    for line in stream:
-#        print(line)
-#        if line.find(admin) in line:
-#           print("Security weakness name: ")
-#           x = x + 1
-#           lines.append(line)
-#    try:
-#      # print(yaml.safe_load(stream))
-#       content = yaml.safe_load(stream)
-#       print(stream.read())
-#       for k, v in content.items():
-#           if "name" in content:
-#               print(k, v)
-
-#    except:
-#        print("exc")
 
 ##[sat_user, sat_user = admin, hammer -u "{{ sat_user }}" -p "{{ sat_pass }}" product create --organization-id "{{ sat_orgid }}" --name "{{ content_rhel_name }}"]
-#'''
