a9b8d7370c7fc2ad23808bb3fa4e384a16803b37
Jackson Hidley <60934202+jackson-hidley@users.noreply.github.com>
2021-11-11 00:11:43 -0600

    Add files via upload

--- Workshop8/Workshop8.py
+++ Workshop8/Workshop8.py
@@ -6,17 +6,22 @@ filei = open("scan-results.json",”r”)
 result = filei.read()
 
 fileo.write("CVE-ID,DESCRIPTION,SEVERITY") 
+while(1):
 
-if "CVE" in filei:
+    if "CVE" in filei:
+        cve = (vuln["CVE"])
+        result = re.search('</i>\n<i>(.*). <',cve)
+        cveResult = result.group(1)   
 
-if "description" in filei:
-            desc = (vuln["description"])
-            result = re.search('</i>\n<i>(.*). <',desc)
-            descResult = result.group(1)
+    if "description" in filei:
+        desc = (vuln["description"])
+        result = re.search('</i>\n<i>(.*). <',desc)
+        descResult = result.group(1)
 
-if "nvdSeverity" in filei:
-    serv = (vuln["nvdSeverity"])
-    result = re.search('</i>\n<i>(.*). <',serv)
-    servResult = result.group(1)
+    if "nvdSeverity" in filei:
+        serv = (vuln["nvdSeverity"])
+        result = re.search('</i>\n<i>(.*). <',serv)
+        servResult = result.group(1)
 
+    fileo.write(cveResult +","+ descResult +","+ servResult )
 fileo.close() 
