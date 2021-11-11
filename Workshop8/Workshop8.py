e08bd69e58c36f36750152fba584e6fd5824e39f
Jackson Hidley <60934202+jackson-hidley@users.noreply.github.com>
2021-11-11 00:05:43 -0600

    Add files via upload

--- Workshop8/Workshop8.py
+++ Workshop8/Workshop8.py
@@ -1,5 +1,22 @@
-file = open(“output.txt”,”w”) 
- 
-file.write("CVE-ID,DESCRIPTION,SEVERITY")
- 
-file.close() 
+import json
+
+fileo = open(“output.txt”,”w”) 
+filei = open("scan-results.json",”r”) 
+
+result = filei.read()
+
+fileo.write("CVE-ID,DESCRIPTION,SEVERITY") 
+
+if "CVE" in filei:
+
+if "description" in filei:
+            desc = (vuln["description"])
+            result = re.search('</i>\n<i>(.*). <',desc)
+            descResult = result.group(1)
+
+if "nvdSeverity" in filei:
+    serv = (vuln["nvdSeverity"])
+    result = re.search('</i>\n<i>(.*). <',serv)
+    servResult = result.group(1)
+
+fileo.close() 
